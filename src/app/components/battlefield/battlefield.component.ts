3536e553c46c9dacdd720b5d623622143468f916
SomethingCreativeStudios <eric-417@live.com>
2018-08-22 00:16:23 -0400

    Fixed App!!!

--- src/app/components/battlefield/battlefield.component.ts
+++ src/app/components/battlefield/battlefield.component.ts
@@ -1,26 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { ElectronService } from '../../providers/electron.service';
-import { debug } from 'util';
 import { WixCard } from '../../models/wixCard';
 import { plainToClass } from 'class-transformer';
 
 @Component({
   selector: 'batoru-battlefield',
   templateUrl: './battlefield.component.html',
-  styleUrls: ['./battlefield.component.scss']
+  styleUrls: ['./battlefield.component.scss'],
 })
 export class BattlefieldComponent implements OnInit {
   private mainDir: string;
 
   constructor(private electronService: ElectronService) {
-    this.loadDir();
+    // this.loadDir();
   }
 
   ngOnInit() {}
 
   private loadDir() {
     this.mainDir = 'C:\\Users\\eric-\\Documents\\Wix Cards\\sets';
-    const data = this.electronService.ipcRenderer.sendSync('load-json', this.mainDir + '\\cards.json');
-    const wixCards = plainToClass(WixCard, data as Object[]);
+    const data = this.electronService.ipcRenderer.sendSync(
+      'load-json',
+      this.mainDir + '\\cards.json',
+    );
+    const wixCards = plainToClass(WixCard, data as object[]);
   }
 }
