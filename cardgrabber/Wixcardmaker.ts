ae6522a9ba189697a1e198685aa77f8ea8fecd9e
Anthony Pierce <Anthonyrpierce95@gmail.com>
2018-08-24 02:01:48 -0400

    added Interface with wixoss card strings

---
+++ cardgrabber/Wixcardmaker.ts
@@ -0,0 +1,27 @@
+
+
+interface IWixCard {
+    name?: string;
+    romanji?: string;
+    color?: string;
+    cardtype?: string;
+    level?: string;
+    limit?: string;
+    limiter?: string;
+    cost?: string;
+    classifier?: string; // LRIG type or LRIG Requirement
+    power?: string;
+    usetiming?: string
+
+
+}
+
+export class CardMaker {
+
+    public getCardFromURL(cardURL){
+        
+        
+
+    } 
+
+}
